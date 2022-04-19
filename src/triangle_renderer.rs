13468717cf81ebb15f4aaa2cc77d821a3d1861e5
Spencer Imbleau <spencer@imbleau.com>
2022-04-19 06:16:39 -0400

    Added method for timing

--- src/triangle_renderer.rs
+++ src/triangle_renderer.rs
@@ -1,3 +1,6 @@
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+
 use crate::tessellator::artifacts::TessellationData;
 use crate::tessellator::tessellator::LyonTessellator;
 
@@ -129,5 +132,66 @@ impl TriangleRenderer {
         }
 
         Ok(())
+    }
+
+    pub fn time(&mut self, frames: usize) -> Result<Vec<Duration>> {
+        let state = self.state.as_mut().unwrap();
+        let window = self.window.as_mut().unwrap();
+        let event_loop = self.event_loop.as_mut().unwrap();
+
+        let mut frame_count = 0;
+        let frame_times = Arc::from(Mutex::from(Vec::<Duration>::new()));
+        let frame_times_arc = frame_times.clone();
+        event_loop.run_return(move |event, _, control_flow| {
+            match event {
+                Event::RedrawRequested(_) => {
+                    let t1 = Instant::now();
+                    match state.render() {
+                        Ok(_) => {
+                            {
+                                let mut frame_times =
+                                    frame_times_arc.lock().unwrap();
+                                frame_times.push(t1.elapsed());
+                            }
+                            frame_count += 1
+                        }
+                        Err(wgpu::SurfaceError::Lost) => {
+                            state.resize(state.size)
+                        }
+                        Err(wgpu::SurfaceError::OutOfMemory) => {
+                            *control_flow = ControlFlow::Exit
+                        }
+                        Err(e) => eprintln!("{:?}", e),
+                    }
+                }
+                Event::MainEventsCleared => {
+                    window.request_redraw();
+                }
+                Event::WindowEvent {
+                    window_id: _,
+                    event,
+                } => match event {
+                    WindowEvent::Resized(size) => state.resize(size),
+                    _ => {}
+                },
+                _ => {}
+            }
+
+            // Return if frames have been drawn
+            if frame_count == frames {
+                *control_flow = ControlFlow::Exit;
+            }
+        });
+        // Unwrap
+        let frame_times =
+            Mutex::into_inner(Arc::try_unwrap(frame_times).unwrap()).unwrap();
+
+        // Ensure all frames were rendered.
+        if frame_times.len() != frames {
+            return Err(NaiveRendererError::FatalRenderingError);
+        }
+
+        // Collect results
+        Ok(frame_times)
     }
 }
